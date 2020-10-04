38cb6b08e108589eb311575443d1cb4ac9f6668a
Jorge Luis <cokimaniac@gmail.com>
2020-10-04 01:53:41 -0400

    Ammount serializers and urls added.

--- debtors/serializers.py
+++ debtors/serializers.py
@@ -23,15 +23,7 @@ class DebtorSerializer(serializers.ModelSerializer):
 
 class AmmountSerializer(serializers.ModelSerializer):
 	"""
-<<<<<<< HEAD
 	Debtor ammount serializer.
 	"""
 	class Meta:
 		fields = ["id", "money", "expected_payment_date", "payment_months", "payment_status", "debtor"]
-=======
-	Ammount of debtor serializer.
-	"""
-	class Meta:
-		fields = ["money", "loan_payment_date"]
->>>>>>> a5a23cd2e0a6d781476588b4303fb2b41861b6b4
-		model = Ammount
--- debtors/urls.py
+++ debtors/urls.py
@@ -23,10 +23,6 @@ urlpatterns = [
 	path(
 		route="<int:id>/ammounts/",
 		name="ammount",
-<<<<<<< HEAD
 		view=views.AmmountView.as_view()
-=======
-		view=views.DebtorAmmountView.as_view()
->>>>>>> a5a23cd2e0a6d781476588b4303fb2b41861b6b4
 	)
 ]
